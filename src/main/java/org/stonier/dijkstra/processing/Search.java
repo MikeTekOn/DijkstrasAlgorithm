053c4ec9e9ba6543e47cae9885968a65490b8c73
brettcalvin <brett.stonier@gmail.com>
2016-12-06 00:27:43 -0800

    Separating initialization from search algorithm

--- src/main/java/org/stonier/dijkstra/processing/Search.java
+++ src/main/java/org/stonier/dijkstra/processing/Search.java
@@ -5,15 +5,16 @@ import org.stonier.dijkstra.model.Graph;
 import org.stonier.dijkstra.model.Path;
 import org.stonier.dijkstra.model.Vertex;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class Search {
-    private static final long INFINITY = Long.MAX_VALUE;
 
     public static Path getShortestPath(Graph graph, Vertex startVertex, Vertex finishVertex) {
 
-        SearchState searchState = initializeGraph(graph, startVertex, finishVertex);
+        SearchState searchState = SearchInitializer.initializeGraph(graph, startVertex, finishVertex);
 
         while (searchState.hasVerticesToVisit()) {
             Optional<VertexWrapper> finishedVertex = visitNextVertexForFinish(searchState);
@@ -24,61 +25,6 @@ public class Search {
         }
 
         throw new IllegalArgumentException("Unable to find path from Start Vertex to Finish Vertex");
-    }
-
-    private static SearchState initializeGraph(Graph graph, Vertex startVertex, Vertex finishVertex) {
-        Map<Vertex, VertexWrapper> wrappedVertices = graph.getVertices().stream()
-                .map(vertex -> wrapVertex(vertex, startVertex))
-                .collect(Collectors.toMap(VertexWrapper::getVertex, vertexWrapper -> vertexWrapper));
-
-        Optional<VertexWrapper> wrappedStartVertex = wrappedVertices.values().stream()
-                .filter(vertexWrapper -> vertexWrapper.getVertex().equals(startVertex))
-                .findFirst();
-        if (!wrappedStartVertex.isPresent()) {
-            throw new IllegalArgumentException("Start Vertex not equal to any Vertex in the Graph");
-        }
-
-        Optional<VertexWrapper> wrappedFinishVertex = wrappedVertices.values().stream()
-                .filter(vertexWrapper -> vertexWrapper.getVertex().equals(finishVertex))
-                .findFirst();
-        if (!wrappedFinishVertex.isPresent()) {
-            throw new IllegalArgumentException("Finish Vertex not equal to any Vertex in the Graph");
-        }
-
-        wrappedVertices.values().forEach(vertex -> wrapVertexEdges(vertex, wrappedVertices));
-
-        return SearchState.builder()
-                .verticesToVisit(wrappedVertices)
-                .startVertex(wrappedStartVertex.get())
-                .finishVertex(wrappedFinishVertex.get())
-                .build();
-    }
-
-    private static VertexWrapper wrapVertex(Vertex vertex, Vertex startVertex) {
-
-        long shortestDistanceFromStart = INFINITY;
-        if (vertex.equals(startVertex)) {
-            shortestDistanceFromStart = 0;
-        }
-
-        return VertexWrapper.builder()
-                .vertex(vertex)
-                .shortestDistanceFromStart(shortestDistanceFromStart)
-                .build();
-    }
-
-    private static void wrapVertexEdges(VertexWrapper vertexWrapper, Map<Vertex, VertexWrapper> vertices) {
-        vertexWrapper.getVertex().getEdges().stream()
-                .map(edge -> wrapEdge(edge, vertices))
-                .forEach(vertexWrapper::addEdge);
-    }
-
-    private static EdgeWrapper wrapEdge(Edge edge, Map<Vertex, VertexWrapper> vertices) {
-
-        return EdgeWrapper.builder()
-                .edge(edge)
-                .connectedVertexWrapper(vertices.get(edge.getConnectedVertex()))
-                .build();
     }
 
     private static Optional<VertexWrapper> visitNextVertexForFinish(SearchState searchState) {
@@ -97,7 +43,7 @@ public class Search {
                 edgeWrapper.getConnectedVertexWrapper().setShortestDistanceFromStart(distanceFromStart);
                 edgeWrapper.getConnectedVertexWrapper().setPreviousEdge(edgeWrapper);
                 edgeWrapper.getConnectedVertexWrapper().setPreviousVertex(vertexToVisit);
-           }
+            }
 
         }
         return Optional.empty();
